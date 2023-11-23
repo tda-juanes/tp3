66b70bea66fe41cbb0fe3655c577571eb78c6eda
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-11-23 02:24:56 -0300

    add linear programming solution

--- main.py
+++ main.py
@@ -1,29 +1,35 @@
 import sys
-import lib.backtracking
-import lib.greedy
 
 def parse_subsets(f):
     return [s.split(',') for s in map(str.strip, f) if s]
 
 if __name__ == "__main__":
     match sys.argv:
-        case [*_, '-'] | [_, '--approx' | '--greedy' | '--exact'] | [_]:
-            subsets = parse_subsets(sys.stdin)
-        case [*_, filename] if not filename.startswith('--'):
-            with open(filename, "r") as f:
+        case [*_, filename] if not filename.startswith('-'):
+            with open(filename, 'r') as f:
                 subsets = parse_subsets(f)
         case _:
-            print(
-                "Usage:",
-                f"    {sys.argv[0]} [--approx | --greedy | --exact] [filename]",
-                file=sys.stderr, sep='\n'
-            )
-            exit(1)
+            subsets = parse_subsets(sys.stdin)
+        # case _:
+        #     print(
+        #         "Usage:",
+        #         f"    {sys.argv[0]} [--linear] [--approx] [filename]",
+        #         file=sys.stderr, sep='\n'
+        #     )
+        #     exit(1)
 
-    if '--approx' in sys.argv or '--greedy' in sys.argv:
-        universal_set = set(sum(subsets, start=[]))
-        sol = lib.greedy.hitting_set(universal_set, subsets)
+    universal_set = set(sum(subsets, start=[]))
+
+    if '--linear' in sys.argv:
+        if '--approx' in sys.argv:
+            from lib.linearprog import hitting_set_approx as hitting_set
+        else:
+            from lib.linearprog import hitting_set_exact as hitting_set
+    elif '--approx' in sys.argv:
+        from lib.greedy import hitting_set
     else:
-        sol = lib.backtracking.hitting_set(subsets)
+        from lib.backtracking import hitting_set
+
+    sol = hitting_set(universal_set, subsets)
     print(f"k = {len(sol)}")
     print(*sol, sep=", ")
