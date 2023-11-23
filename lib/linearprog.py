1e06092cba6c5177562e4e2149125c74894f65af
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-11-23 02:23:24 -0300

    too many values to unpack

--- lib/linearprog.py
+++ lib/linearprog.py
@@ -1,25 +1,30 @@
 import pulp
 from pulp import LpVariable, LpProblem, lpSum, LpMinimize
 
-def hitting_set(A, subsets, variable_cat):
-    binary_vars = {item: LpVariable(item, cat=variable_cat) for item in A}
+def hitting_set(A, subsets, category):
+    binary_vars = {item: LpVariable(item, cat=category) for item in A}
     problem = LpProblem('Hitting Set', LpMinimize)
+
     for subset in subsets:
         subset_items = list(map(binary_vars.get, subset))
         problem += lpSum(subset_items) >= 1
+
     problem += lpSum(list(binary_vars.values()))
     problem.solve()
-    return {item.getName(): pulp.value(item) for item in binary_vars.values()}
+
+    return {item: pulp.value(var) for item, var in binary_vars.items()}
 
 """
-Algoritmo por programación lineal entera que obtiene la solución óptima al problema.
+Algoritmo por programación lineal entera que obtiene la solución óptima al
+problema.
 """
 def hitting_set_exact(A, subsets):
     result = hitting_set(A, subsets, 'Binary')
-    return [item for item, value in result if value > 0]
+    return [item for (item, value) in result.items() if value > 0]
 
 """
-Algoritmo por programación lineal que obtiene una solución aproximada al problema.
+Algoritmo por programación lineal que obtiene una solución aproximada al
+problema.
 """
 def hitting_set_approx(A, subsets):
     result = hitting_set(A, subsets, 'Integer')
