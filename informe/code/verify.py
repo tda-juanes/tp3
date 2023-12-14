44d4a113a87fd592504db94ffc20efc4cb318af9
Juan Ignacio Díaz <diazjignacio@gmail.com>
2023-12-14 16:11:21 -0300

    definicion y complejidad

--- informe/code/verify.py
+++ informe/code/verify.py
@@ -1,6 +1,9 @@
 def is_hitting_set(A, B, C, k):
     if len(C) > k:
         return False
+    for c in C:
+        if c not in A:
+            return False
     for Bi in B:
         for b in Bi:
             if b in C:
